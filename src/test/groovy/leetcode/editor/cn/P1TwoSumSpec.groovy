94eb11632b4a58f8d47d4e94d519ff5f43d04753
techzealot <techzealot@foxmail.com>
2021-03-12 13:12:34 +0800

    separate inputs and expected outputs with ||

--- src/test/groovy/leetcode/editor/cn/P1TwoSumSpec.groovy
+++ src/test/groovy/leetcode/editor/cn/P1TwoSumSpec.groovy
@@ -17,11 +17,11 @@ class P1TwoSumSpec extends Specification {
             twoSum(arrs, target) == expect
         }
         where: "data-driven test,All failures will be reported"
-        arrs                    | target | expect
-        [2, 7, 11, 15] as int[] | 9      | [0, 1] as int[]
-        [3, 2, 4] as int[]      | 6      | [1, 2] as int[]
-        [3, 3] as int[]         | 6      | [0, 1] as int[]
-        [] as int[]             | 0      | [0] as int[]
-        [1] as int[]            | 1      | [0] as int[]
+        arrs                    | target || expect
+        [2, 7, 11, 15] as int[] | 9      || [0, 1] as int[]
+        [3, 2, 4] as int[]      | 6      || [1, 2] as int[]
+        [3, 3] as int[]         | 6      || [0, 1] as int[]
+        [] as int[]             | 0      || [0] as int[]
+        [1] as int[]            | 1      || [0] as int[]
     }
 }
