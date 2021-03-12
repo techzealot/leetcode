a85bdc8d52516f5dc6c75d05819b1bd6d6a60b1c
techzealot <techzealot@foxmail.com>
2021-03-12 13:09:38 +0800

    use shared test object

--- src/test/groovy/leetcode/editor/cn/P1TwoSumSpec.groovy
+++ src/test/groovy/leetcode/editor/cn/P1TwoSumSpec.groovy
@@ -1,17 +1,22 @@
 package leetcode.editor.cn
 
+import spock.lang.Shared
 import spock.lang.Specification
 
 class P1TwoSumSpec extends Specification {
+
+    @Shared
+    P1TwoSum p1TwoSum = new P1TwoSum();
+    @Shared
+    P1TwoSum.Solution solution = new P1TwoSum.Solution(p1TwoSum)
+
     def "int array: #arrs ,target: #target ,twoSum: #expect"() {
-        given:
-        P1TwoSum p1TwoSum = new P1TwoSum();
-        P1TwoSum.Solution solution = new P1TwoSum.Solution(p1TwoSum)
-        expect:
+        given: "init solution object"
+        expect: "use with to verify actual and expect,fails on the first failed assertions,use verifyAll to soft assert"
         with(solution) {
             twoSum(arrs, target) == expect
         }
-        where:
+        where: "data-driven test,All failures will be reported"
         arrs                    | target | expect
         [2, 7, 11, 15] as int[] | 9      | [0, 1] as int[]
         [3, 2, 4] as int[]      | 6      | [1, 2] as int[]
